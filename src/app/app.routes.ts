6d021a33cbb55f599a7792dffd58032e7b9f41a7
Server-Booster <dicrescemanuel@gmail.com>
2023-12-13 09:50:11 +0100

    Project structure refactored

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./pages/search/search.page').then( m => m.SearchPage)
   },
 ];
